21da7da5439a9b2a54bac1f3c6671ed8af50dd0a
Victor Portugal <victorpibrahim@gmail.com>
2023-09-14 10:58:13 -0300

    Alteracao de comentario

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -69,7 +69,7 @@ public class King extends ChessPiece {
 			mat[p.getRow()][p.getCol()] = true;
 		}
 
-		// nw
+		// se
 		p.setValues(position.getRow() + 1, position.getCol() - 1);
 		if (getBoard().positionExists(p) && canMove(p)) {
 			mat[p.getRow()][p.getCol()] = true;
