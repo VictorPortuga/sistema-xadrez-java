4c781010795de61eaf9862b35ba4d5c0b306257f
Victor Portugal <victorpibrahim@gmail.com>
2023-09-18 14:05:54 -0300

    Jogada especial - Promocao

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,12 @@ public class Program {
 				if( capturedPiece != null ) {
 					captured.add(capturedPiece);
 				}
+				
+				if( chessMatch.getPromoted() != null ) {
+					System.out.print("Insira a peça para a promoção (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			
 			}
 			catch( ChessException e ) {
